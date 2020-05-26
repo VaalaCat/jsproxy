07c669f2393876cbe0d8ebf84283c32b62f25fb6
Vaala Cat <vaalacat@outlook.com>
2020-05-26 13:45:23 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -12,12 +12,12 @@ jsproxy_config({
 
   // 节点配置
   node_map: {
-    'mysite': {
-      label: '当前站点',
-      lines: {
-        [location.host]: 1,
-      }
-    },
+    //'mysite': {
+    //  label: '当前站点',
+    //  lines: {
+    //    [location.host]: 1,
+    //  }
+    //},
     // 该节点用于加载大体积的静态资源
     'cfworker': {
       label: 'cfworker',
